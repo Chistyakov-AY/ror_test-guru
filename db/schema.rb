c72803068dc652d48f6565ccd447b10301144d4f
Aleksander <chistyakov_ay@mail.ru>
2023-10-17 22:25:26 +0300

    Добалено ограниечение по целостности в tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_17_133753) do
+ActiveRecord::Schema.define(version: 2023_10_17_164630) do
 
   create_table "tests", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.integer "level"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
