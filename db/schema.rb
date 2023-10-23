d60f91d8f4b3592248e6bf93df0da0de6f6cc736
Aleksander <chistyakov_ay@mail.ru>
2023-10-23 18:49:06 +0300

    Миграция всех таблиц

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_10_23_131745) do
+ActiveRecord::Schema.define(version: 2023_10_23_154646) do
+
+  create_table "answers", force: :cascade do |t|
+    t.string "body", null: false
+    t.boolean "correct", default: false
+    t.integer "question_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "body", null: false
+    t.integer "test_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["test_id"], name: "index_questions_on_test_id"
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "title", null: false
@@ -21,5 +46,14 @@ ActiveRecord::Schema.define(version: 2023_10_23_131745) do
     t.index ["category_id"], name: "index_tests_on_category_id"
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "title", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "answers", "questions"
+  add_foreign_key "categories", "users"
+  add_foreign_key "questions", "tests"
   add_foreign_key "tests", "categories"
 end
