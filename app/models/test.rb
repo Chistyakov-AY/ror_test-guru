153dbdc7b894c2b9fb6aa493141f3cf689672be9
Aleksander <chistyakov_ay@mail.ru>
2023-11-14 12:08:12 +0300

    Убрал интерполяцию в методе класса

--- app/models/test.rb
+++ app/models/test.rb
@@ -7,6 +7,6 @@ class Test < ApplicationRecord
   belongs_to :category
 
   def self.tests_by_category(category_title)
-    joins(:category).where("categories.title = ?", "#{category_title}").order('id DESC')
+    joins(:category).where("categories.title = ?", category_title).order('id DESC')
   end    
 end
