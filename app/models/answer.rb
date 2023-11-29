9c6efad04a5b02eb088d5c25053ec6d6313055c6
Aleksander <chistyakov_ay@mail.ru>
2023-11-29 16:17:16 +0300

    изменение названия метода validate_answers_to_questions

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,12 +1,12 @@
 class Answer < ApplicationRecord
   validates :body, presence: true
-  validate :limiting_reporting_for_questions
+  validate :validate_answers_to_questions
 
   belongs_to :question
 
   scope :right_answers, -> { where(correct: true) }
 
-  def limiting_reporting_for_questions
+  def validate_answers_to_questions
     errors.add(:question) if question.answers.count >= 4
   end
 end
