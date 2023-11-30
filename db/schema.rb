92577634a433a614b040c67b53503a97e1d31b85
Aleksander <chistyakov_ay@mail.ru>
2023-11-30 12:51:57 +0300

    Add index to tests

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_11_24_132926) do
+ActiveRecord::Schema.define(version: 2023_11_30_094653) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2023_11_24_132926) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "category_id"
     t.integer "user_id"
+    t.index ["title", "level"], name: "index_tests_on_title_and_level", unique: true
     t.index ["user_id"], name: "index_tests_on_user_id"
   end
 
