40833d39d0568a9b4245d8dcd914473a8241e8ed
Aleksander <chistyakov_ay@mail.ru>
2024-01-18 11:45:53 +0300

    Lesson 8 complet completed

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -4,6 +4,14 @@ module QuestionsHelper
   end
 
   def github_url(author, repo)
-    link_to "GitHub репозиторий", "https://github.com/#{author}/#{repo}", target: :blank
+    link_to "GitHub репозиторий", "https://github.com/#{author}/#{repo}/", target: :blank
+  end
+
+  def question_header(question)
+    if question.persisted?
+      "Edit question '#{question.body}'"
+    else
+      "Create New Questions"
+    end
   end
 end
