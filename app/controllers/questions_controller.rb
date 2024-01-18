f8b6dbd6ca175fc7ea989599bcfee9ea4a32fb46
Aleksander <chistyakov_ay@mail.ru>
2024-01-18 11:14:06 +0300

    intermediate result

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,34 +1,43 @@
 class QuestionsController < ApplicationController
-  before_action :find_question, only: %i[show destroy]
-  before_action :find_test, only: %i[index create]
+  before_action :find_question, only: %i[show destroy edit update]
+  before_action :find_test, only: %i[index new create]
 
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_question_not_found
   
-  def index
-    # @questions = Question.all
-    # render plain: @test.questions.inspect
-  end
+  # def index
+  # end
   
-  def show
-    render plain: @question.body
+  # def show
+  # end
+
+  def edit
   end
 
   def new
-
+    @question = @test.questions.new
   end
 
   def create
     @question = @test.questions.new(question_params)
 
     if @question.save
-      render inline: '<p>Вопрос: <%= @question.inspect %>!</p> добавлен!'
+      render :show
     else
-      render inline: '<p>Ошибка: <%= @question.errors.inspect %>!</p>'
+      render :new
+    end
+  end
+
+  def update
+    if @question.update(question_params)
+      redirect_to question_path
+    else
+      render :edit
     end
   end
 
   def destroy
     @question.destroy
+    redirect_to test_questions_path(@question.test_id)
   end
 
   private
--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,9 @@
 module QuestionsHelper
+  def current_year
+    Date.today.year
+  end
+
+  def github_url(author, repo)
+    link_to "GitHub репозиторий", "https://www.github.com/#{author}/#{repo}/tree/master", target: :blank
+  end
 end
