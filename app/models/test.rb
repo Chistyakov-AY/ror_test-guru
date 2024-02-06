bf31fc06d7def5a1a2df323ffebea1477b39b99d
Aleksander <chistyakov_ay@mail.ru>
2024-02-06 15:02:35 +0300

    after collbacks in Model

--- app/models/test.rb
+++ app/models/test.rb
@@ -10,9 +10,6 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
 
-  # has_many :test_users
-  # has_many :users, through: :test_users
-
   scope :list_of_level_tests, -> (level) { where(level: level) }
   scope :simple_tests, -> { list_of_level_tests(0..1) }
   scope :average_tests, -> { list_of_level_tests(2..4) }
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -1,14 +1,41 @@
 class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
-  # belongs_to :question
-  belongs_to :current_question, class_name: 'Question', foreign_key: "question_id", optional: true
+  belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
 
-private
+  def completed?
+    current_question.nil?
+  end
+  
+  def accept!(answer_ids)
+    if correct_answer?
+      self.correct_questions += 1
+    end
+  
+    self.current_question = next_question
+    save!
+  end
+  
+  private
+  
+  def correct_answer?(answer_ids)
+    correct_answers_count = correct_answers.count
+  
+    (correct_answers_count == correct_answers.where(id: answer_ids).count) &&
+    correct_answers_count == answer_ids.count
+  end
+  
+  def correct_answers
+    current_question.answers.correct
+  end
+  
+  def next_question
+    test.questions.order(:id).where('id > ?', current_question.id).first
+  end
 
   def before_validation_set_first_question
-    self.current_question = test.question.first if test.present?
+    self.current_question = test.questions.first if test.present?
   end
 end
