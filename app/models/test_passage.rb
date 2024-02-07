7da83956de82d431767036d167b96df4a967a665
Aleksander <chistyakov_ay@mail.ru>
2024-02-07 12:47:19 +0300

    created next question in model TestPasage with callback

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -4,6 +4,7 @@ class TestPassage < ApplicationRecord
   belongs_to :current_question, class_name: 'Question', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
+  before_validation :before_validation_next_question, on: :update
 
   def completed?
     current_question.nil?
@@ -13,8 +14,7 @@ class TestPassage < ApplicationRecord
     if correct_answer?(answer_ids)
       self.correct_questions += 1
     end
-  
-    self.current_question = next_question
+
     save!
   end
   
@@ -37,5 +37,9 @@ class TestPassage < ApplicationRecord
 
   def before_validation_set_first_question
     self.current_question = test.questions.first if test.present?
+  end
+
+  def before_validation_next_question
+    self.current_question = next_question  
   end
 end
