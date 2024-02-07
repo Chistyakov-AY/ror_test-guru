1543fe30908fdeac3ff7e09abe3ce270e1ba6e84
Aleksander <chistyakov_ay@mail.ru>
2024-02-07 21:21:34 +0300

    first commit in lesson 10

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_01_29_094657) do
+ActiveRecord::Schema.define(version: 2024_02_07_182011) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2024_01_29_094657) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "password_digest"
   end
 
   add_foreign_key "answers", "questions"
