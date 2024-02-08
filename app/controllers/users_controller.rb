3327250994014a6939762013e3fbe1044ad9b74d
Aleksander <chistyakov_ay@mail.ru>
2024-02-08 23:39:07 +0300

    creating new users via action index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to tests_path, notice: "User '#{@user.title}' was successfully create."
+      redirect_to users_path, notice: "User '#{@user.title}' was successfully created."
     else
       render :new
     end
