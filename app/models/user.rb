a37ab1340f2ab17bcc2f0ebf398cf29d22df9068
Aleksander <chistyakov_ay@mail.ru>
2024-02-08 15:55:59 +0300

    created user's template with authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,14 @@
+require 'digest/sha1'
+
 class User < ApplicationRecord
   
-  validates :title, presence: true
-  validates :email, presence: true
+  include Auth
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: "Test"
-  # has_many :test_users
-  # has_many :tests, through: :test_users
 
-  scope :easy, -> (title) { where(title: title) }
-
-  def list_of_user_tests(level)
-    tests.list_of_level_tests(level)
-  end
+  # has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
