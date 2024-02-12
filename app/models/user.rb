28283d1c2de1ea332000b7e2e107f01913a64460
Aleksander <chistyakov_ay@mail.ru>
2024-02-12 21:49:22 +0300

    install gem bcrypt (3.1.20) for has_secure_password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,16 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
+  validates :title, presence: true
+  validates :email, presence: true, uniqueness: true
   
-  include Auth
+  # include Auth
 
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: "Test"
 
-  # has_secure_password
+  has_secure_password
 
   def test_passage(test)
     test_passages.order(id: :desc).find_by(test_id: test.id)
