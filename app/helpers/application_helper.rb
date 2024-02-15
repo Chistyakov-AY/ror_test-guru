bc542840277fac6e60be29861e8a098420d31dd1
Aleksander <chistyakov_ay@mail.ru>
2024-02-15 14:04:12 +0300

    lesson10 to be tested

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,8 @@ module ApplicationHelper
   def github_url(author, repo)
     link_to "GitHub репозиторий", "https://github.com/#{author}/#{repo}/", target: :blank
   end
+
+  def flash_messages
+    content_tag :p, flash[:alert], class: 'flash alert' if flash[:alert]
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 require 'digest/sha1'
 
 class User < ApplicationRecord
-  validates :title, presence: true
-  validates :email, presence: true, uniqueness: true
   
-  # include Auth
-
+  EMAIL_FORMAT = /[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+/
+  
+  validates :title, presence: true
+  validates :email, presence: true, 
+                    uniqueness: true,
+                    format: { with: EMAIL_FORMAT }
+  
   has_many :test_passages
   has_many :tests, through: :test_passages
   has_many :author_tests, class_name: "Test"
