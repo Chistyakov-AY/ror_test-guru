0adf59520538967325ee8d986b5338afe2cd3fe2
Aleksander <chistyakov_ay@mail.ru>
2024-03-25 22:21:40 +0300

    delete/link_to worked

--- app/controllers/admin/questions_controller.rb
+++ app/controllers/admin/questions_controller.rb
@@ -1,4 +1,4 @@
-class QuestionsController < ApplicationController
+class Admin::QuestionsController < Admin::BaseController
   before_action :find_question, only: %i[show destroy edit update]
   before_action :find_test, only: %i[new create]
 
