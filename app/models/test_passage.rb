17ad997e0483f1d037628ac55e920ee693e538fa
Aleksander <chistyakov_ay@mail.ru>
2024-05-06 06:55:47 +0300

    del an unnecessary association

--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -2,7 +2,6 @@ class TestPassage < ApplicationRecord
   belongs_to :user
   belongs_to :test
   belongs_to :current_question, class_name: 'Question', optional: true
-  belongs_to :current_user, class_name: 'User', optional: true
 
   before_validation :before_validation_set_first_question, on: :create
   before_validation :before_validation_set_next_question, on: :update
