34d3c9ba85217a32903087748030c7329df790c5
Aleksander <chistyakov_ay@mail.ru>
2024-05-28 20:01:29 +0300

    Lesson 16 completed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,12 +3,15 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+import "jquery"
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'utilities/sorting'
 import 'utilities/checkup_password'
+import 'utilities/form_inline'
+import 'utilities/progress_bar'
 
 Rails.start()
 Turbolinks.start()
